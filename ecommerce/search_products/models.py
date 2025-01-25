bbe957ba0782e614192a8f57484e63bebf92b7fa
karanDevX11 <bistakaran89@gmail.com>
2025-01-25 17:11:31 +0545

    Add is_featured field in model and update views

--- ecommerce/search_products/models.py
+++ ecommerce/search_products/models.py
@@ -16,6 +16,7 @@ class Product(models.Model):
     name = models.CharField(max_length=100)
     category = models.ForeignKey(Category, on_delete= models.CASCADE, related_name= "products")
     description = models.TextField(blank=True, null=True)
+    is_featured = models.BooleanField(blank=False, null=False, , default=False)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.PositiveBigIntegerField(default=0)
     image = models.ImageField(upload_to="product_images/", blank=True, null=True)
--- ecommerce/search_products/views.py
+++ ecommerce/search_products/views.py
@@ -18,12 +18,22 @@ class HomeView(ListView):
 class ProductView(FormView, ListView):
     template_name = "search_products/product_form.html"
     form_class = ProductForm
-    success_url = "/products/"  # Redirect after successful form submission
+    success_url = "home"  # Redirect after successful form submission
     context_object_name = "featured_products"
+
+    def get_queryset(self):
+        # Customize the queryset for featured products
+        return Product.objects.filter(is_featured=True)
 
     def form_valid(self, form):
         form.save()
         return super().form_valid(form)
+    
+    def get_context_data(self, **kwargs):
+        # Add both the form and the featured products to the context
+        context = super().get_context_data(**kwargs)
+        context['form'] = self.get_form()  # Add the form to the context
+        return context
 
 
 
