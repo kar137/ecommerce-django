38a1ed62a0a97d4f9d42f829fc7a7b5696e8754c
karanDevX11 <bistakaran89@gmail.com>
2025-02-02 22:50:56 +0545

    Update User model to avoid conflicts with django builtin user auth

--- ecommerce/accounts/models.py
+++ ecommerce/accounts/models.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, Group, Permission
 from django.db import models
 
 # Create your models here.
@@ -6,6 +6,17 @@ from django.db import models
 class User(AbstractUser):
     profile_pic = models.ImageField(upload_to="profile_pics/", blank= True, null= True)
     address = models.CharField(max_length=255, blank= True, null= True)
+
+    groups = models.ManyToManyField(
+        Group,
+        related_name="custom_user_groups",  # Avoids conflict
+        blank=True
+    )
+    user_permissions = models.ManyToManyField(
+        Permission,
+        related_name="custom_user_permissions",  # Avoids conflict
+        blank=True
+    )
 
     def __str__(self):
         return self.username
